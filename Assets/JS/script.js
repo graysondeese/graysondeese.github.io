bf2ab187ba592a99345b7c2ffe98b17f62c44fd7
Grayson Deese <gldeese@ncsu.edu>
2020-06-17 02:40:09 -0400

    Fixed animation on hamburger menu.. need to work on coloring on hover.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,10 +2,16 @@
 const menuIcon = document.querySelector(".hamburgerMenu");
 const logo = document.querySelector(".logo");
 const navbar = document.querySelector(".navbar");
+let line1 = document.querySelector(".line-1");
+let line2 = document.querySelector(".line-2");
+let line3 = document.querySelector(".line-3");
 
 // Logic for hamburger menu
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('change');
+    line1.classList.toggle('rotate1');
+    line2.classList.toggle('rotate2');
+    line3.classList.toggle('rotate3');
 })
 // function to make navbar and logo disappear on scroll down and appear on scroll up
 // add logic to stop the function if mw < 850
@@ -27,7 +33,20 @@ var currentScrollPos = window.pageYOffset;
   prevScrollPos = currentScrollPos;
 }
 
+// making all lines change color when the user hovers over one line
+menuIcon.addEventListener("mouseover", function( event ) {
+  line1.style.backgroundColor = '#66ff66';
+  line2.style.backgroundColor = '#66ff66';
+  line3.style.backgroundColor = '#66ff66';
 
+// reset the color after a short delay
+setTimeout(function() {
+  line1.style.backgroundColor = '#fff';
+  line2.style.backgroundColor = '#fff';
+  line3.style.backgroundColor = '#fff';
+}, 500);
 
-// media query
-const mw = window.matchMedia("(max-width: 850px)")
+})
+
+// === media query ===
+// const mw = window.matchMedia("(max-width: 850px)")
