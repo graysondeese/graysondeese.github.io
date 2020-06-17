5a94beb36ff31ded1fa0a9c34ca2e84607a3118b
Grayson Deese <gldeese@ncsu.edu>
2020-06-17 01:59:36 -0400

    added js logic for header and organzied some code.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,21 +1,33 @@
-// Logic for hamburger menu
+// Setting all my vars
 const menuIcon = document.querySelector(".hamburgerMenu");
-const logo = document.querySelector(".logo a");
+const logo = document.querySelector(".logo");
 const navbar = document.querySelector(".navbar");
 
+// Logic for hamburger menu
 menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('change')
+    navbar.classList.toggle('change');
 })
 // function to make navbar and logo disappear on scroll down and appear on scroll up
-var prevScrollpos = window.pageYOffset;
+// add logic to stop the function if mw < 850
+var prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
+  $(".navbar").removeClass('change')
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+// if we are scrolling down
+  if (prevScrollPos > currentScrollPos) {
+    // $(".navbar").hide();
     menuIcon.style.top = "20px";
     logo.style.top = "9px";
+    // if we are not scrolling down
   } else {
+    // $(".navbar").show();
     menuIcon.style.top = "-50px";
     logo.style.top = "-50px";
   }
-  prevScrollpos = currentScrollPos;
+  prevScrollPos = currentScrollPos;
 }
+
+
+
+// media query
+const mw = window.matchMedia("(max-width: 850px)")
